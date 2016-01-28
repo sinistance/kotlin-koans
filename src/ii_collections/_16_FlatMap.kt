9d2b0d004b4831f22b70a901ffe418032e9eca76
sinistance <sinistance@gmail.com>
2016-01-28 21:11:07 +0700

    bab 2

--- src/ii_collections/_16_FlatMap.kt
+++ src/ii_collections/_16_FlatMap.kt
@@ -1,5 +1,8 @@
 package ii_collections
 
+import ii_collections.data.customers
+import ii_collections.data.orderedProducts
+
 fun example() {
 
     val result = listOf("abc", "12").flatMap { it.toList() }
@@ -9,10 +12,10 @@ fun example() {
 
 val Customer.orderedProducts: Set<Product> get() {
     // Return all products ordered by customer
-    todoCollectionTask()
+    return orders.flatMap { it.products }.toSet()
 }
 
 val Shop.allOrderedProducts: Set<Product> get() {
     // Return all products that were ordered by at least one customer
-    todoCollectionTask()
+    return customers.flatMap { it.orderedProducts }.toSet()
 }
